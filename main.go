b7a2e1c92328becf38531312fdb94090c28bce2d
Andrei K <andrei@12ak.com>
2022-05-03 22:31:22 -0400

    Parse template

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+// tpl is a container holding all the parsed templates
+var tpl *template.Template
+
+func init() {
+	// Must() does error checking and returns the template
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
