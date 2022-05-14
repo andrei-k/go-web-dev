2e8e867c8ed90adb6a20f2fb54370cf7499c0d6c
Andrei K <andrei@12ak.com>
2022-05-13 21:40:58 -0400

    Use index to reference items in a struct

--- main.go
+++ main.go
@@ -4,38 +4,26 @@ import (
 	"log"
 	"os"
 	"text/template"
-	"time"
 )
 
-// tpl is a container holding all the parsed templates
 var tpl *template.Template
 
 func init() {
-	// Must() does error checking and returns the template
-	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("templates/*"))
-}
-
-func monthDayYear(t time.Time) string {
-	// Time.Format accepts a layout in this syntax: 01/02 03:04:05PM '06 -0700
-	// In my case, I want:
-	// day (referred to as 02)
-	// month (referred to as 01)
-	// year (referred to as 06)
-	return t.Format("02-01-2006")
-}
-
-func convertString(s string) string {
-	return s + "!"
-}
-
-// Create a FuncMap to register functions
-var fm = template.FuncMap{
-	"fdateMDY":      monthDayYear,
-	"convertString": convertString,
+	tpl = template.Must(template.ParseFiles("./templates/index.gohtml"))
 }
 
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", time.Now())
+	xs := []string{"zero", "one", "two", "three", "four", "five"}
+
+	data := struct {
+		Words []string
+		Name  string
+	}{
+		xs,
+		"James Bond",
+	}
+
+	err := tpl.Execute(os.Stdout, data)
 	if err != nil {
 		log.Fatalln(err)
 	}
