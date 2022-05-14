0fe92a6af8536dfabaa0ac40c24e3c1b08a5c4d3
Andrei K <andrei@12ak.com>
2022-05-13 22:08:43 -0400

    Build calendar

--- main.go
+++ main.go
@@ -6,14 +6,46 @@ import (
 	"text/template"
 )
 
+type course struct {
+	Number, Name, Units string
+}
+
+type semester struct {
+	Term    string
+	Courses []course
+}
+
+type cal struct {
+	Fall, Spring, Summer semester
+}
+
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 }
 
 func main() {
-	err := tpl.Execute(os.Stdout, "Bob")
+	cal := cal{
+		Fall: semester{
+			Term: "Fall",
+			Courses: []course{
+				{"CSCI-40", "Introduction to Programming in Go", "4"},
+				{"CSCI-130", "Introduction to Web Programming with Go", "4"},
+				{"CSCI-140", "Mobile Apps Using Go", "4"},
+			},
+		},
+		Spring: semester{
+			Term: "Spring",
+			Courses: []course{
+				{"CSCI-50", "Advanced Go", "5"},
+				{"CSCI-190", "Advanced Web Programming with Go", "5"},
+				{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+			},
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, cal)
 	if err != nil {
 		log.Fatalln(err)
 	}
