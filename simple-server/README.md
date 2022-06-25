f888d8f337010492599a22896f66be7d329ee29c
Andrei K <andrei@12ak.com>
2022-06-24 22:58:25 -0400

    Add simple-server

---
+++ simple-server/README.md
@@ -0,0 +1,14 @@
+# Simple Server with Go
+
+Practice setting up a simple server following this [tutorial](https://www.youtube.com/watch?v=jFfo23yIWac).
+
+## Usage
+
+Run in Terminal
+
+```
+go run .
+```
+
+Open the following link in the browser
+http://localhost:8080/
---
+++ simple-server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.NotFound(w, r)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Post request successful\n")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Email: %s\n", email)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
